9a3e71db44e7c73012bbc376106b35b1e6a45f1d
bluss <bluss@users.noreply.github.com>
2016-08-14 11:32:26 +0200

    Update docs

---
+++ master/implementors/serde/de/trait.Deserialize.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["ndarray"] = ["impl&lt;A,&nbsp;Di,&nbsp;S&gt; <a class='trait' href='https://serde-rs.github.io/serde/serde/de/trait.Deserialize.html' title='serde::de::Deserialize'>Deserialize</a> for <a class='struct' href='ndarray/struct.ArrayBase.html' title='ndarray::ArrayBase'>ArrayBase</a>&lt;S,&nbsp;Di&gt; <span class='where'>where A: <a class='trait' href='https://serde-rs.github.io/serde/serde/de/trait.Deserialize.html' title='serde::de::Deserialize'>Deserialize</a>, Di: <a class='trait' href='https://serde-rs.github.io/serde/serde/de/trait.Deserialize.html' title='serde::de::Deserialize'>Deserialize</a> + <a class='trait' href='ndarray/trait.Dimension.html' title='ndarray::Dimension'>Dimension</a>, S: <a class='trait' href='ndarray/trait.DataOwned.html' title='ndarray::DataOwned'>DataOwned</a>&lt;Elem=A&gt;</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
